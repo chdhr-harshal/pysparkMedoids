846e01f51febf26464f3f9a710f831e754e91602
Harshal Chaudhari <harshal@bu.edu>
2018-07-07 22:21:41 -0700

    Added url in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='pysparkMedoids',
       version='0.1.0',
       description='kMedoids clustering in pySpark for a user supplied distance function',
-      url='',
+      url='https://github.com/chdhr-harshal/pysparkMedoids',
       author='Harshal A. Chaudhari',
       author_email='harshal@bu.edu',
       license='MIT',
